b19c85139e6430b3ec4d344aa524f6f736693f6d
Marc R. Hoffmann <hoffmann@mountainminds.com>
2006-08-28 20:07:13 +0000

    Copyright headers.

--- com.mountainminds.eclemma.ui/src/com/mountainminds/eclemma/internal/ui/annotation/CoverageAnnotation.java
+++ com.mountainminds.eclemma.ui/src/com/mountainminds/eclemma/internal/ui/annotation/CoverageAnnotation.java
@@ -1,6 +1,10 @@
-/*
+/*******************************************************************************
+ * Copyright (c) 2006 Mountainminds GmbH & Co. KG
+ * This software is provided under the terms of the Eclipse Public License v1.0
+ * See http://www.eclipse.org/legal/epl-v10.html.
+ *
  * $Id$
- */
+ ******************************************************************************/
 package com.mountainminds.eclemma.internal.ui.annotation;
 
 import org.eclipse.jface.text.Position;
