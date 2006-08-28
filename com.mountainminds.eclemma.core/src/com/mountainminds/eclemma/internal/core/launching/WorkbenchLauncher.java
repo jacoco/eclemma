7a525519978969464a672d148aa786f7db81c0c4
Marc R. Hoffmann <hoffmann@mountainminds.com>
2006-08-28 20:06:31 +0000

    Copyright headers.

--- com.mountainminds.eclemma.core/src/com/mountainminds/eclemma/internal/core/launching/WorkbenchLauncher.java
+++ com.mountainminds.eclemma.core/src/com/mountainminds/eclemma/internal/core/launching/WorkbenchLauncher.java
@@ -1,6 +1,10 @@
-/*
- * $Id: JUnitPluginLauncher.java 32 2006-08-23 15:45:27Z mho $
- */
+/*******************************************************************************
+ * Copyright (c) 2006 Mountainminds GmbH & Co. KG
+ * This software is provided under the terms of the Eclipse Public License v1.0
+ * See http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * $Id: $
+ ******************************************************************************/
 package com.mountainminds.eclemma.internal.core.launching;
 
 import org.eclipse.core.runtime.CoreException;
