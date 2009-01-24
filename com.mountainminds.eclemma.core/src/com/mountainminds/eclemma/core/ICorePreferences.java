fd2d597b0fe40c4867b70b55dd8ced7890ea1978
Marc R. Hoffmann <hoffmann@mountainminds.com>
2009-01-24 21:40:01 +0000

    SF #2226932, #2080402: Additional preferences for default instrumentation behavior.

--- com.mountainminds.eclemma.core/src/com/mountainminds/eclemma/core/ICorePreferences.java
+++ com.mountainminds.eclemma.core/src/com/mountainminds/eclemma/core/ICorePreferences.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2007 Mountainminds GmbH & Co. KG
+ * Copyright (c) 2006, 2009 Mountainminds GmbH & Co. KG
  * This software is provided under the terms of the Eclipse Public License v1.0
  * See http://www.eclipse.org/legal/epl-v10.html.
  *
@@ -9,9 +9,9 @@ package com.mountainminds.eclemma.core;
 
 /**
  * Clients may implement this interface to customize the behavior of the EclEmma
- * core plug-in and pass a instance to 
- * {@link com.mountainminds.eclemma.core.CoverageTools#setPreferences(ICorePreferences)}.
- * This interface decouples the core e.g. from the UI preferences.
+ * core plug-in and pass a instance to
+ * {@link CoverageTools#setPreferences(ICorePreferences)}. This interface
+ * decouples the core e.g. from the UI preferences.
  * 
  * @author Marc R. Hoffmann
  * @version $Revision: $
@@ -22,23 +22,67 @@ public interface ICorePreferences {
    * Default behavior if no customization is set.
    */
   public static final ICorePreferences DEFAULT = new ICorePreferences() {
-    public boolean getActivateNewSessions() { return true; }
-    public boolean getAutoRemoveSessions() { return false; }
+    public boolean getActivateNewSessions() {
+      return true;
+    }
+
+    public boolean getAutoRemoveSessions() {
+      return false;
+    }
+
+    public boolean getDefaultInstrumentationSourceFoldersOnly() {
+      return true;
+    }
+
+    public boolean getDefaultInstrumentationSameProjectOnly() {
+      return false;
+    }
+
+    public String getDefaultInstrumentationFilter() {
+      return "";//$NON-NLS-1$
+    }
   };
-  
+
   /**
    * Determines whether new sessions should automatically be activated.
    * 
-   * @return true, if sessions should be activated
+   * @return <code>true</code>, if sessions should be activated
    */
   public boolean getActivateNewSessions();
-  
+
   /**
    * Determines whether sessions should automatically be removed when their
-   * respective lauch is removed from the debug environment
-   *   
-   * @return true, if sessions should be removed
+   * respective launch is removed from the debug environment.
+   * 
+   * @return <code>true</code>, if sessions should be removed
    */
   public boolean getAutoRemoveSessions();
-  
+
+  /**
+   * Specification of the default instrumentation behavior: Instrument source
+   * folders only.
+   * 
+   * @return <code>true</code>, if source folders only should be instrumented by
+   *         default
+   */
+  public boolean getDefaultInstrumentationSourceFoldersOnly();
+
+  /**
+   * Specification of the default instrumentation behavior: Instrument code in
+   * the same project only. This filter works only for launch configuration
+   * types that have a reference to a project.
+   * 
+   * @return <code>true</code>, if code in the same project should be
+   *         instrumented only
+   */
+  public boolean getDefaultInstrumentationSameProjectOnly();
+
+  /**
+   * Returns a comma separated list of match strings that specifies patterns for
+   * class path entries to be instrumented by default.
+   * 
+   * @return List of match strings
+   */
+  public String getDefaultInstrumentationFilter();
+
 }
--- com.mountainminds.eclemma.ui/src/com/mountainminds/eclemma/internal/ui/UIPreferences.java
+++ com.mountainminds.eclemma.ui/src/com/mountainminds/eclemma/internal/ui/UIPreferences.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 Mountainminds GmbH & Co. KG
+ * Copyright (c) 2006, 2009 Mountainminds GmbH & Co. KG
  * This software is provided under the terms of the Eclipse Public License v1.0
  * See http://www.eclipse.org/legal/epl-v10.html.
  *
@@ -16,42 +16,76 @@ import com.mountainminds.eclemma.core.ICorePreferences;
 /**
  * Constants and initializer for the preference store.
  * 
- * @author  Marc R. Hoffmann
+ * @author Marc R. Hoffmann
  * @version $Revision$
  */
 public class UIPreferences extends AbstractPreferenceInitializer {
 
   public static final String PREF_SHOW_COVERAGE_VIEW = EclEmmaUIPlugin.ID
-     + ".show_coverage_view"; //$NON-NLS-1$
-  
+      + ".show_coverage_view"; //$NON-NLS-1$
+
   public static final String PREF_ACTICATE_NEW_SESSIONS = EclEmmaUIPlugin.ID
-    + ".activate_new_sessions"; //$NON-NLS-1$ 
+      + ".activate_new_sessions"; //$NON-NLS-1$ 
+
+  public static final String PREF_DEFAULT_INSTRUMENTATION_SOURCE_FOLDERS_ONLY = EclEmmaUIPlugin.ID
+      + ".default_instrumentation_source_folders_only"; //$NON-NLS-1$ 
+
+  public static final String PREF_DEFAULT_INSTRUMENTATION_SAME_PROJECT_ONLY = EclEmmaUIPlugin.ID
+      + ".default_instrumentation_same_project_only"; //$NON-NLS-1$ 
+
+  public static final String PREF_DEFAULT_INSTRUMENTATION_FILTER = EclEmmaUIPlugin.ID
+      + ".default_instrumentation_filter"; //$NON-NLS-1$ 
 
   public static final String PREF_AUTO_REMOVE_SESSIONS = EclEmmaUIPlugin.ID
-    + ".auto_remove_sessions"; //$NON-NLS-1$ 
-  
+      + ".auto_remove_sessions"; //$NON-NLS-1$ 
+
   public static final String PREF_ALLOW_INPLACE_INSTRUMENTATION = EclEmmaUIPlugin.ID
-    + ".allow_inplace_instrumentation"; //$NON-NLS-1$ 
-  
+      + ".allow_inplace_instrumentation"; //$NON-NLS-1$ 
+
   public static final ICorePreferences CORE_PREFERENCES = new ICorePreferences() {
     public boolean getActivateNewSessions() {
       return getPreferenceStore().getBoolean(PREF_ACTICATE_NEW_SESSIONS);
     }
+
     public boolean getAutoRemoveSessions() {
       return getPreferenceStore().getBoolean(PREF_AUTO_REMOVE_SESSIONS);
+    }
+
+    public boolean getDefaultInstrumentationSourceFoldersOnly() {
+      return getPreferenceStore().getBoolean(
+          PREF_DEFAULT_INSTRUMENTATION_SOURCE_FOLDERS_ONLY);
+    }
+
+    public boolean getDefaultInstrumentationSameProjectOnly() {
+      return getPreferenceStore().getBoolean(
+          PREF_DEFAULT_INSTRUMENTATION_SAME_PROJECT_ONLY);
+    }
+
+    public String getDefaultInstrumentationFilter() {
+      return getPreferenceStore()
+          .getString(PREF_DEFAULT_INSTRUMENTATION_FILTER);
     }
   };
 
   public void initializeDefaultPreferences() {
     IPreferenceStore pref = getPreferenceStore();
     pref.setDefault(PREF_SHOW_COVERAGE_VIEW, true);
-    pref.setDefault(PREF_ACTICATE_NEW_SESSIONS, ICorePreferences.DEFAULT.getActivateNewSessions());
-    pref.setDefault(PREF_AUTO_REMOVE_SESSIONS, ICorePreferences.DEFAULT.getAutoRemoveSessions());
-    pref.setDefault(PREF_ALLOW_INPLACE_INSTRUMENTATION, MessageDialogWithToggle.PROMPT);
+    pref.setDefault(PREF_ACTICATE_NEW_SESSIONS, ICorePreferences.DEFAULT
+        .getActivateNewSessions());
+    pref.setDefault(PREF_AUTO_REMOVE_SESSIONS, ICorePreferences.DEFAULT
+        .getAutoRemoveSessions());
+    pref.setDefault(PREF_DEFAULT_INSTRUMENTATION_SOURCE_FOLDERS_ONLY,
+        ICorePreferences.DEFAULT.getDefaultInstrumentationSourceFoldersOnly());
+    pref.setDefault(PREF_DEFAULT_INSTRUMENTATION_SAME_PROJECT_ONLY,
+        ICorePreferences.DEFAULT.getDefaultInstrumentationSameProjectOnly());
+    pref.setDefault(PREF_DEFAULT_INSTRUMENTATION_FILTER,
+        ICorePreferences.DEFAULT.getDefaultInstrumentationFilter());
+    pref.setDefault(PREF_ALLOW_INPLACE_INSTRUMENTATION,
+        MessageDialogWithToggle.PROMPT);
   }
 
   private static IPreferenceStore getPreferenceStore() {
     return EclEmmaUIPlugin.getInstance().getPreferenceStore();
   }
-  
+
 }
